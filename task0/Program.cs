14b186b55f33994e93aa546cd16cd95cf790d7dd
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-29 13:18:57 +0300

    Add task0

---
+++ task0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Square of a number
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int sqnumber = number * number;
+Console.WriteLine("Square of number " + number + " equals " + sqnumber);
+
