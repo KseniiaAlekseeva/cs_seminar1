e344dc8f8ff4918f92fe4bb40c406337345ba59d
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-29 13:34:01 +0300

    Add task1 my version

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// If first number is a square of second?
+
+Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("If first number is a square of second?");
+
+if (a == b * b) Console.WriteLine("Yes");
+else Console.WriteLine("No");
