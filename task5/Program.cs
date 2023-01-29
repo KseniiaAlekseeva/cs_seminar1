ce9a5e0fe44af14319043499e4e6273dbab580c9
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-29 14:14:47 +0300

    Add task5 my version

---
+++ task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Print all integer number in the range (-N, N)
+
+Console.WriteLine("Enter an integer number: ");
+
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("!!! " + N);
+
+int count = 0;
+int currNum;
+
+if (N > 0) currNum = -N;
+else currNum = N;
+
+Console.WriteLine("&& " + currNum);
+
+
+while (count < Math.Abs(N) * 2 + 1)
+{
+    Console.Write(currNum + ", ");
+    currNum = currNum + 1;
+    count++;
+}
